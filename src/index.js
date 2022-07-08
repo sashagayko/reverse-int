17bf127ba4dba541cda25dbccf6ad357bc772e6e
Sasha <hlupik69@gmail.com>
2022-07-08 23:52:36 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str = n + '';
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      if (str[i] == '-') {
+        newStr = newStr;
+      } else {
+        newStr = newStr + str[i]
+      }
+    } 
+    return +newStr;
 }
